b83e0dd2d1c8b30b28e3efc855fe99a7e57a7eea
Solomon Ince <53124218+Solonce@users.noreply.github.com>
2024-03-26 14:21:05 -0400

    Delete static directory

--- static/js/getTime.js
+++
@@ -1,8 +0,0 @@
-// Get the current date and time
-let currentDateTime = new Date();
-
-// Format the date and time as a string
-let formattedDateTime = currentDateTime.toLocaleString();
-
-// Display the formatted date and time in the HTML element with id "currentDateTime"
-document.getElementById("currentDateTime").textContent = formattedDateTime;
